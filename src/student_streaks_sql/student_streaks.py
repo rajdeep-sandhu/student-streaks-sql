34b2e742d8d2f1a41a5451d90943fdfb50f5c0bc
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-06-14 08:06:10 +0000

    create student_streaks.py

---
+++ src/student_streaks_sql/student_streaks.py
@@ -0,0 +1,13 @@
+import marimo
+
+__generated_with = "0.23.9"
+app = marimo.App(width="full", app_title="Student Streaks")
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
